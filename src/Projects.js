fefc8cd7aa2965812095ae249a6b8d01d4b329f4
ricardo <rfigueroa2317@gmail.com>
2025-01-30 01:16:33 -0500

    adding project list

--- src/Projects.js
+++ src/Projects.js
@@ -1,7 +1,16 @@
 import React from "react";
+import "./Projects.css";
 
 const Projects = () => {
-  return <h3>Personal Projects</h3>;
+  return (
+    <>
+      <h3>Personal Projects</h3>
+      <ul>
+        <li className="line">Angular Fitness Tracker</li>
+        <li className="line">Roxy's Little Creations</li>
+      </ul>
+    </>
+  );
 };
 
 export default Projects;
